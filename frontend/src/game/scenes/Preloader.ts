d6297c763b1bd4d01c1b22b35b2e8d4b378a3d38
FPLCodes <sahabferdous1@gmail.com>
2025-02-08 02:25:28 +0800

    add new map to phaser

--- frontend/src/game/scenes/Preloader.ts
+++ frontend/src/game/scenes/Preloader.ts
@@ -6,8 +6,6 @@ export class Preloader extends Scene {
     }
 
     init() {
-        this.add.image(512, 384, "background");
-
         this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
 
         const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
