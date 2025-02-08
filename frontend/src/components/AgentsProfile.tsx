43199175e1a52ab7d47838e532f20aa0924eb299
FPLCodes <sahabferdous1@gmail.com>
2025-02-09 03:07:54 +0800

    fixing more ts errors for deployment

--- frontend/src/components/AgentsProfile.tsx
+++ frontend/src/components/AgentsProfile.tsx
@@ -26,7 +26,6 @@ export default function AgentsProfile() {
     return (
         <ProfileCard
             address={agentAddress}
-            data={data}
             profileType="agent"
             infoRows={[
                 {
